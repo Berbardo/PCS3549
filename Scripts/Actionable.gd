4de9b6e465d613ef0709012a53b57cb6434beba0
Berbardo <bernardo.rcoutinho@gmail.com>
2023-12-07 00:27:39 -0300

    Add new main character and light sources

--- Scripts/Actionable.gd
+++ Scripts/Actionable.gd
@@ -1,7 +1,7 @@
 extends Area2D
 
 @export var dialogue_resource: DialogueResource
-@export var dialogue_start: String = "start"
+@export var dialogue_start: String = "ddr"
 
 func action() -> void:
 	DialogueManager.show_example_dialogue_balloon(dialogue_resource, dialogue_start)
